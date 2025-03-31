613ebe154d2f39e2cc19ae0d47e102bd4a83ddd0
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2025-03-31 20:45:45 +0530

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -5,7 +5,7 @@ def quick_sort(arr):
     pivot = arr[len(arr) // 2]  # Choose the middle element as pivot
     left = [x for x in arr if x < pivot]     # Elements less than pivot
     middle = [x for x in arr if x == pivot]  # Elements equal to pivot
-    right = [x for x in arr if x > pivot]    # Elements greater than pivot
+    right = [x for x in arr if x > pivot]    # Elements greaterthan pivot
 
     return quick_sort(left) + middle + quick_sort(right)
 
